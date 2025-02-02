10510b529adc658c7da629887d8f1e0210ae4f91
Greg <gregcourt10@gmail.com>
2025-02-02 13:48:14 +0000

    run again azure perms (test2)

--- terraform/resource_groups.tf
+++ terraform/resource_groups.tf
@@ -1,4 +1,4 @@
 resource "azurerm_resource_group" "this" {
-  name     = "rg-test1-${var.env}-${var.location_short}-01"
+  name     = "rg-test2-${var.env}-${var.location_short}-01"
   location = var.location
 }
