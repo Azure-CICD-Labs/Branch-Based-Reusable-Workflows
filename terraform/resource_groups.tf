4a5ddc9c30e6dd26edb218ed3675afe07b4e3bc1
Greg <gregcourt10@gmail.com>
2025-02-02 15:17:21 +0000

    added debug + 02

--- terraform/resource_groups.tf
+++ terraform/resource_groups.tf
@@ -1,4 +1,4 @@
 resource "azurerm_resource_group" "this" {
-  name     = "rg-bbrw-${var.env}-${var.location_short}-01"
+  name     = "rg-bbrw-${var.env}-${var.location_short}-02"
   location = var.location
 }
