29ccf5ec9dbb275852714d9dadbfd0dace360a2f
Greg <gregcourt10@gmail.com>
2025-02-02 15:35:43 +0000

    myfeature

--- terraform/resource_groups.tf
+++ terraform/resource_groups.tf
@@ -1,4 +1,9 @@
 resource "azurerm_resource_group" "this" {
   name     = "rg-bbrw-${var.env}-${var.location_short}-02"
   location = var.location
+  tags = {
+    feature = "myfeature"
+  }
 }
+
+# feature
